5e829d59d3d0e05fed1c062b0fbbe1878380d220
materight <matteo.est@gmail.com>
2022-04-13 21:57:58 +0200

    Fix dataset

--- actions/actions.py
+++ actions/actions.py
@@ -1,24 +1,20 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
+"""Custom actions."""
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+# Load dataset globally
+dataset = Dataset()
 
-class ActionHelloWorld(Action):
+class ActionSearchByTitle(Action):
+    """Search for a recipe by title."""
 
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_search_by_title"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         dispatcher.utter_message(text="Hello World!")
-
         return []
+
+
--- actions/dataset.py
+++ actions/dataset.py
@@ -43,39 +43,51 @@ class Dataset():
         with open(recipes_path, 'r') as f:
             raw_recipes = yaml.load(f, Loader=yaml.FullLoader)
         # Convert to DataFrame
-        self.recipes = pd.DataFrame([ dict(id=i, **r) for i, r in enumerate(raw_recipes)])
-        self.ingredients = pd.DataFrame([ dict(recipe_id=i, **ingr) for i, r in enumerate(raw_recipes) for ingr in r['ingredients'] ])
-        self.steps = pd.DataFrame([ dict(recipe_id=i, step_index=j, description=desc) for i, r in enumerate(raw_recipes) for j, desc in enumerate(r['steps']) ])
+        self._df_recipes = pd.DataFrame([ dict(id=i, **r) for i, r in enumerate(raw_recipes)])
+        self._df_ingredients = pd.DataFrame([ dict(recipe_id=i, **ingr) for i, r in enumerate(raw_recipes) for ingr in r['ingredients'] ])
+        self._df_steps = pd.DataFrame([ dict(recipe_id=i, step_index=j, description=desc) for i, r in enumerate(raw_recipes) for j, desc in enumerate(r['steps']) ])
         # Post-processing
-        self.recipes = self.recipes.drop(['ingredients', 'steps'], axis=1).set_index('id')
-        self.ingredients[['amount', 'unit']] = self.ingredients.amount.fillna('').astype(str).str.split(r'(\d+)(.*)', expand=True)[[1,2]].replace('', None)
-        self.ingredients['amount'] = self.ingredients['amount'].astype(float)
-
+        self._df_recipes = self._df_recipes.drop(['ingredients', 'steps'], axis=1).set_index('id')
+        self._df_ingredients[['amount', 'unit']] = self._df_ingredients.amount.fillna('').astype(str).str.split(r'(\d+)(.*)', expand=True)[[1,2]].replace('', None)
+        self._df_ingredients['amount'] = self._df_ingredients['amount'].astype(float)
 
     def _get_recipes(self, recipe_ids: List[int]) -> List[Recipe]:
         """Converts a list of recipe ids to a list of Recipe objects."""
         recipes = []
         for recipe_id in recipe_ids:
-            recipe_df = self.recipes.loc[recipe_id]
-            ingredients_df = self.ingredients[self.ingredients.recipe_id == recipe_id]
-            steps_df = self.steps[self.steps.recipe_id == recipe_id].sort_values(by='step_index')
-            ingredients = [ Ingredient(**ingr) for ingr in ingredients_df.to_dict('records') ]
-            steps = [ Step(**step) for step in steps_df.to_dict('records') ]
-            recipe = Recipe(id=recipe_id, **recipe_df, ingredients=ingredients, steps=steps)
+            df_recipe = self._df_recipes.loc[recipe_id]
+            df_ingredients = self._df_ingredients[self._df_ingredients.recipe_id == recipe_id]
+            df_steps = self._df_steps[self._df_steps.recipe_id == recipe_id].sort_values(by='step_index')
+            ingredients = [ Ingredient(**ingr) for ingr in df_ingredients.to_dict('records') ]
+            steps = [ Step(**step) for step in df_steps.to_dict('records') ]
+            recipe = Recipe(id=recipe_id, **df_recipe, ingredients=ingredients, steps=steps)
             recipes.append(recipe)
         return recipes
 
+    @property
+    def recipes(self) -> List[Text]:
+        """Returns a list of all the available recipes titles."""
+        return self._df_recipes.title.unique().tolist()
+
+    @property
+    def ingredients(self) -> List[Text]:
+        """Returns a list of all the available ingredients."""
+        return self._df_ingredients.name.unique().tolist()
+
+    @property
+    def ingredient_units(self) -> List[Text]:
+        """Returns a list of all the available ingredient units."""
+        return self._df_ingredients.unit.unique().tolist()
 
     def search_by_title(self, query: Text) -> List[Recipe]:
         """Search for a recipe by title."""
-        recipes_ids = self.recipes[self.recipes['title'].str.contains(query, case=False)].index.to_list()
+        recipes_ids = self._df_recipes[self._df_recipes['title'].str.contains(query, case=False)].index.to_list()
         return self._get_recipes(recipes_ids)
-
 
     def search_by_ingredients(self, query: List[Text]) -> List[Recipe]:
         """Search for a recipe by ingredients."""
         query = '|'.join(query)
-        results = self.ingredients[self.ingredients['name'].str.contains(query)]
+        results = self._df_ingredients[self._df_ingredients['name'].str.contains(query)]
         # Count number of ingredients occurences
         results = results.groupby('recipe_id', as_index=False).name.count().rename(columns={'name': 'count'})
         recipes_ids = results.sort_values('count', ascending=False).recipe_id.to_list()
