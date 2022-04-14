ae9115fff12ba5278619d3a671e81eadb2edbcea
materight <matteo.est@gmail.com>
2022-04-14 21:35:49 +0200

    Update

--- actions/actions.py
+++ actions/actions.py
@@ -2,7 +2,10 @@
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
+
+from .dataset import Dataset
 
 # Load dataset globally
 dataset = Dataset()
@@ -11,10 +14,18 @@ class ActionSearchByTitle(Action):
     """Search for a recipe by title."""
 
     def name(self) -> Text:
-        return "action_search_by_title"
+        return 'action_search_by_keyword'
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="Hello World!")
-        return []
+        recipe_keyword = tracker.get_latest_entity_values('recipe_keyword', None)
+        
+        recipe_results = dataset.search_by_keyword(recipe_keyword)
+
+        if len(recipe_results) == 0:
+            dispatcher.utter_message(template='utter_recipe_not_found')
+        else:
+            dispatcher.utter_message(template='utter_recipe_found')
+            return [SlotSet('found_recipes', recipe_results)]
+
 
 
--- actions/dataset.py
+++ actions/dataset.py
@@ -51,19 +51,6 @@ class Dataset():
         self._df_ingredients[['amount', 'unit']] = self._df_ingredients.amount.fillna('').astype(str).str.split(r'(\d+)(.*)', expand=True)[[1,2]].replace('', None)
         self._df_ingredients['amount'] = self._df_ingredients['amount'].astype(float)
 
-    def _get_recipes(self, recipe_ids: List[int]) -> List[Recipe]:
-        """Converts a list of recipe ids to a list of Recipe objects."""
-        recipes = []
-        for recipe_id in recipe_ids:
-            df_recipe = self._df_recipes.loc[recipe_id]
-            df_ingredients = self._df_ingredients[self._df_ingredients.recipe_id == recipe_id]
-            df_steps = self._df_steps[self._df_steps.recipe_id == recipe_id].sort_values(by='step_index')
-            ingredients = [ Ingredient(**ingr) for ingr in df_ingredients.to_dict('records') ]
-            steps = [ Step(**step) for step in df_steps.to_dict('records') ]
-            recipe = Recipe(id=recipe_id, **df_recipe, ingredients=ingredients, steps=steps)
-            recipes.append(recipe)
-        return recipes
-
     @property
     def recipes(self) -> List[Text]:
         """Returns a list of all the available recipes titles."""
@@ -74,23 +61,26 @@ class Dataset():
         """Returns a list of all the available ingredients."""
         return self._df_ingredients.name.unique().tolist()
 
-    def search_by_title(self, query: Text) -> List[Recipe]:
-        """Search for a recipe by title."""
-        recipes_ids = self._df_recipes[self._df_recipes['title'].str.contains(query, case=False)].index.to_list()
-        return self._get_recipes(recipes_ids)
+    def get_recipe(self, recipe_id: int) -> Recipe:
+        """Converts a recipe id to the corresponding Recipe objects."""
+        df_recipe = self._df_recipes.loc[recipe_id]
+        df_ingredients = self._df_ingredients[self._df_ingredients.recipe_id == recipe_id]
+        df_steps = self._df_steps[self._df_steps.recipe_id == recipe_id].sort_values(by='step_index')
+        ingredients = [ Ingredient(**ingr) for ingr in df_ingredients.to_dict('records') ]
+        steps = [ Step(**step) for step in df_steps.to_dict('records') ]
+        recipe = Recipe(id=recipe_id, **df_recipe, ingredients=ingredients, steps=steps)
+        return recipe
 
-    def search_by_ingredients(self, query: List[Text]) -> List[Recipe]:
+    def search_by_keyword(self, query: Text) -> List[int]:
+        """Search for a recipe by keyword."""
+        recipes_ids = self._df_recipes[self._df_recipes['title'].str.contains(query, case=False)].index.to_list()
+        return recipes_ids
+
+    def search_by_ingredients(self, query: List[Text]) -> List[int]:
         """Search for a recipe by ingredients."""
         query = '|'.join(query)
         results = self._df_ingredients[self._df_ingredients['name'].str.contains(query)]
         # Count number of ingredients occurences
         results = results.groupby('recipe_id', as_index=False).name.count().rename(columns={'name': 'count'})
         recipes_ids = results.sort_values('count', ascending=False).recipe_id.to_list()
-        return self._get_recipes(recipes_ids)
-
-
-if __name__ == '__main__':
-    dataset = Dataset()
-    by_title = dataset.search_by_title('pasta')
-    by_ingr = dataset.search_by_ingredients(['egg', 'cheese'])
-    print('Dataset loaded')
+        return recipes_ids
