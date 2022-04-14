715a31325e4a009a865a8a9751999f8ac058fac8
materight <matteo.est@gmail.com>
2022-04-14 18:07:45 +0200

    Fix dataset generation

--- generate_data.py
+++ generate_data.py
@@ -1,6 +1,6 @@
 """Simple script to generate data using chatette and convert it to the rasa YAML format."""
 import os
-import yaml
+import shutil
 from chatette.facade import Facade as ChatetteFacade
 from rasa.nlu.convert import convert_training_data
 
@@ -17,11 +17,16 @@ chatette.run()
 # Read Chatette output and convert it to the yaml format supported by rasa
 print('Converting data to YAML format...')
 convert_training_data(os.path.join(DATA_DIR, 'chatette', '.out'), os.path.join(DATA_DIR, 'rasa', 'nlu.yml'), 'yml', 'en')
+shutil.rmtree(os.path.join(DATA_DIR, 'chatette', '.out'))
 
 # Generate base file for chatette with entity synonyms and lookup tables
 print('Generating lookup tables and regex from dataset...')
 dataset = Dataset()
 with open(os.path.join(DATA_DIR, 'rasa', 'ingredients.yml'), 'w', encoding='utf-8') as file:
-    yaml.dump(dict(version='3.1', nlu=[
-        dict(lookup='ingredients', examples=dataset.ingredients)
-    ]), file, sort_keys=False, allow_unicode=True)
+    # Write ingredients in rasa format (with yaml.dump is not possible to get the correct formatting)
+    file.write('version: \'3.1\'\n'
+               'nlu:\n'
+               '- lookup: ingredients\n'
+               '  examples: |\n')
+    for ingredient in dataset.ingredients:
+        file.write(f'    - {ingredient}\n')
